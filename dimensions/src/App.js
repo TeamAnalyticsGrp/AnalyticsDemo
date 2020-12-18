a32cbe81c6d4cc98526adac8b6d3260e32eb29a8
sharmaanuraj <35272172+sharmaanuraj@users.noreply.github.com>
2020-12-19 00:33:09 +0530

    grid

--- dimensions/src/App.js
+++ dimensions/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
+import categories from './categories.json';
+import products from './products.json';
+import { process } from '@progress/kendo-data-query';
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import { DropDownList } from '@progress/kendo-react-dropdowns';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+
+  state = {
+    dropdownlistCategory: null,
+    gridDataState: {
+      sort: [
+        { field: "ProductName", dir: "asc" }
+      ],
+      page: { skip: 0, take: 10 }
+    }
+  }
+
+  handleDropDownChange = (e) => {
+    let newDataState = { ...this.state.gridDataState }
+    if (e.target.value.CategoryID !== null) {
+      newDataState.filter = {
+        logic: 'and',
+        filters: [{ field: 'CategoryID', operator: 'eq', value: e.target.value.CategoryID }]
+      }
+      newDataState.skip = 0
+    } else {
+      newDataState.filter = []
+      newDataState.skip = 0
+    }
+    this.setState({
+      dropdownlistCategory: e.target.value.CategoryID,
+      gridDataState: newDataState
+    });
+  }
+
+  handleGridDataStateChange = (e) => {
+    this.setState({gridDataState: e.data});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello KendoReact!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <DropDownList
+            data={categories}
+            dataItemKey="CategoryID"
+            textField="CategoryName"
+            defaultItem={{CategoryID: null, CategoryName: "Product categories"}}
+            onChange={this.handleDropDownChange}
+            />
+          &nbsp; Selected category ID: <strong>{this.state.dropdownlistCategory}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <Grid
+          data={process(products, this.state.gridDataState)}
+          pageable={true}
+          sortable={true}
+          {...this.state.gridDataState}
+          onDataStateChange={this.handleGridDataStateChange}
+          style={{ height: "400px" }}>
+          <GridColumn field="ProductName" title="Product Name" />
+          <GridColumn field="UnitPrice" title="Price" format="{0:c}" />
+          <GridColumn field="UnitsInStock" title="Units in Stock" />
+          <GridColumn field="Discontinued" cell={checkboxColumn} />
+        </Grid>
+      </div>
+    );
+  }
+}
+
+class checkboxColumn extends Component {
+  render() {
+    return (
+        <td>
+          <input type="checkbox" checked={this.props.dataItem[this.props.field]} disabled="disabled" />
+        </td>
+    );
+  }
 }
 
 export default App;
