a99004fe96976b145546f80cb66bc3018591b39b
Dwaipayan Bhattacharya <dwaipayanb@anthology.com>
2020-12-21 18:12:44 +0530

    Navbar and footer

--- dimensions/src/Navbar.js
+++ dimensions/src/Navbar.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import LogoFull from './logo/logo_full.png';
 
 class Navbar extends Component {
     render() {
@@ -9,14 +10,16 @@ class Navbar extends Component {
                 <header className="home-header">
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <div className="container-fluid">
-                            <a className="navbar-brand" href="#">Dimensions</a>
+                            <a href="#" >
+                                <img src={LogoFull} width="auto" height="50px%"></img>
+                            </a>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarText">
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                        <a className="nav-link active" aria-current="page" href="#">Email</a>
                                     </li>
                                     <li className="nav-item">
                                         <a className="nav-link" href="#">Donations</a>
@@ -25,12 +28,13 @@ class Navbar extends Component {
                                         <a className="nav-link" href="#">Fees</a>
                                     </li>
                                 </ul>
-                                <span className="navbar-text text-left">
-                                    Hi, Anuraj
-                            </span>
                             </div>
                         </div>
+                        <div className="navbar-text text-right">
+                            Hi, Anuraj
+                        </div>
                     </nav>
+
                 </header>
             </div>
         );
