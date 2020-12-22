d2856b025d1c39a78eabbbb7a044f4b256615e73
sharmaanuraj <35272172+sharmaanuraj@users.noreply.github.com>
2020-12-22 15:10:59 +0530

    DataGrid with sorting filtering and paging working

--- dimensions/src/DataGrid.js
+++ dimensions/src/DataGrid.js
@@ -1,84 +1,80 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import categories from './categories.json';
 import products from './products.json';
 import { process } from '@progress/kendo-data-query';
 import { Grid, GridColumn } from '@progress/kendo-react-grid';
-import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { orderBy } from '@progress/kendo-data-query';
+import { filterBy } from '@progress/kendo-data-query';
 import '@progress/kendo-theme-default/dist/all.css';
+
+
+
 
 class DataGrid extends Component {
 
-    state = {
-      dropdownlistCategory: null,
-      gridDataState: {
-        sort: [
-          { field: "ProductName", dir: "asc" }
-        ],
-        page: { skip: 0, take: 10 }
-      }
-    }
-  
-    handleDropDownChange = (e) => {
-      let newDataState = { ...this.state.gridDataState }
-      if (e.target.value.CategoryID !== null) {
-        newDataState.filter = {
-          logic: 'and',
-          filters: [{ field: 'CategoryID', operator: 'eq', value: e.target.value.CategoryID }]
-        }
-        newDataState.skip = 0
-      } else {
-        newDataState.filter = []
-        newDataState.skip = 0
-      }
-      this.setState({
-        dropdownlistCategory: e.target.value.CategoryID,
-        gridDataState: newDataState
-      });
-    }
-  
-    handleGridDataStateChange = (e) => {
-      this.setState({gridDataState: e.data});
-    }
-  
-    render() {
-      return (
-        <div className="Grid">          
-          <p>
-            <DropDownList
-              data={categories}
-              dataItemKey="CategoryID"
-              textField="CategoryName"
-              defaultItem={{CategoryID: null, CategoryName: "Product categories"}}
-              onChange={this.handleDropDownChange}
-              />
-            &nbsp; Selected category ID: <strong>{this.state.dropdownlistCategory}</strong>
-          </p>
-  
-          <Grid
-            data={process(products, this.state.gridDataState)}
-            pageable={true}
-            sortable={true}
-            {...this.state.gridDataState}
-            onDataStateChange={this.handleGridDataStateChange}
-            style={{ height: "400px" }}>
-            <GridColumn field="ProductName" title="Product Name" />
-            <GridColumn field="UnitPrice" title="Price" format="{0:c}" />
-            <GridColumn field="UnitsInStock" title="Units in Stock" />
-            <GridColumn field="Discontinued" cell={checkboxColumn} />
-          </Grid>
-        </div>
-      );
-    }
+  state = {
+    skip: 0, take: 10,
+    filter: {
+      logic: "and",
+      filters: [
+        { field: "ProductName", operator: "contains", value: "Tea" }
+      ]
+    },
+    sort: [
+      { field: 'ProductName', dir: 'asc' }
+    ]
   }
-  
-  class checkboxColumn extends Component {
-    render() {
-      return (
-          <td>
-            <input type="checkbox" checked={this.props.dataItem[this.props.field]} disabled="disabled" />
-          </td>
-      );
-    }
+  pageChange = (event) => {
+    this.setState({
+      skip: event.page.skip,
+      take: event.page.take
+    });
   }
-  
-  export default DataGrid;
+
+
+  render() {
+    return (
+      <div className="Grid">
+        <Grid
+          data={filterBy(orderBy(products.slice(this.state.skip, this.state.take + this.state.skip), this.state.sort),this.state.filter)}
+          skip={this.state.skip}
+          take={this.state.take}
+          total={products.length}
+          pageable={true}
+          sortable={true}
+          sort={this.state.sort}
+          filterable={true}
+          filter={this.state.filter}
+          onSortChange={(e) => {
+            this.setState({
+              sort: e.sort
+            });
+          }}
+          onFilterChange={(e) => {
+            this.setState({
+                filter: e.filter
+            });
+        }}
+          onPageChange={this.pageChange}
+          style={{ height: "400px" }}>
+          <GridColumn field="ProductName" title="Product Name" />
+          <GridColumn field="UnitPrice" title="Price" format="{0:c}" />
+          <GridColumn field="UnitsInStock" title="Units in Stock" />
+          <GridColumn field="Discontinued" cell={checkboxColumn} />
+        </Grid>
+      </div>
+    );
+  }
+}
+
+class checkboxColumn extends Component {
+  render() {
+    return (
+      <td>
+        <input type="checkbox" checked={this.props.dataItem[this.props.field]} disabled="disabled" />
+      </td>
+    );
+  }
+}
+
+export default DataGrid;
